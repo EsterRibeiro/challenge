1c3801e3fa43af79696331c4408fd5812329eb8c
EsterRibeiro <ester.ribeiro10101@gmail.com>
2024-12-06 16:09:19 -0300

    adicionando variável de ambiente

--- Lora.Take.Domain/Services/GitHubService.cs
+++ Lora.Take.Domain/Services/GitHubService.cs
@@ -1,6 +1,8 @@
 ﻿using Lora.Take.Domain.Interfaces.Services;
 using Lora.Take.Domain.Models;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Lora.Take.Domain.Services;
 public class GitHubService : IGitHubService
@@ -8,9 +10,11 @@ public class GitHubService : IGitHubService
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly string _githubToken;
 
-    public GitHubService(IHttpClientFactory httpClient)
+    public GitHubService(IHttpClientFactory httpClient, IConfiguration configuration)
     {
         _httpClientFactory = httpClient;
+        _githubToken = Environment.GetEnvironmentVariable("GitHub_ApiToken")
+                ?? throw new InvalidOperationException("O token da API GitHub não foi configurado.");
     }
 
     /// <summary>
@@ -24,11 +28,9 @@ public class GitHubService : IGitHubService
         var url = $"https://api.github.com/orgs/{repositoryName}/repos";
         var request = new HttpRequestMessage(HttpMethod.Get, url);
 
-        //user agent
         request.Headers.Add("User-Agent", "DotNetApp");
         request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _githubToken);
 
-        //envia a requisição
         var client = _httpClientFactory.CreateClient("github");
 
         var response = client.Send(request);
